c2f0217f2f13d93439546064b482fceee3f5766e
amir-repository <achmadnassardhi@gmail.com>
2024-01-04 02:26:43 +0700

    Add schedule resource filament

---
+++ app/Filament/Resources/ScheduleResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ScheduleResource\Pages;
+use App\Filament\Resources\ScheduleResource\RelationManagers;
+use App\Models\Day;
+use App\Models\Schedule;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TimePicker;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ScheduleResource extends Resource
+{
+    protected static ?string $model = Schedule::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
+
+    protected static ?string $navigationGroup = 'Schedule Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('lecturer_id')
+                    ->label('Lecturer')
+                    ->options(User::role('lecturer')->get()->pluck('name', 'id'))
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+                Select::make('user_id')
+                    ->label('Student')
+                    ->options(User::role('student')->get()->pluck('name', 'id'))
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+                Select::make('day_id')
+                    ->label('Day')
+                    ->options(Day::all()->pluck('name', 'id'))
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+                TimePicker::make('time')->prefix('Starts')
+                    ->required(),
+                TextInput::make('duration')->label('Duration (minutes)')->numeric()->required()->default(90),
+                TextInput::make('link')->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('lecturer.name'),
+                TextColumn::make('user.name'),
+                TextColumn::make('day.name'),
+                TextColumn::make('time'),
+                TextColumn::make('duration')->label('Duration (minutes)'),
+                TextColumn::make('link'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchedules::route('/'),
+            'create' => Pages\CreateSchedule::route('/create'),
+            'edit' => Pages\EditSchedule::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ScheduleResource/Pages/CreateSchedule.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\ScheduleResource\Pages;
+
+use App\Filament\Resources\ScheduleResource;
+use App\Models\Schedule;
+use Carbon\Carbon;
+use Dflydev\DotAccessData\Data;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+
+class CreateSchedule extends CreateRecord
+{
+    protected static string $resource = ScheduleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $lecturerID = $data['lecturer_id'];
+        $dayID = $data['day_id'];
+
+        $schedules = Schedule::where(['lecturer_id' => $lecturerID, 'day_id' => $dayID])->get();
+
+        $schedules->each(function ($schedule) use ($data) {
+            // check if a new schedules starttime and endtime in between of existing schedule
+
+            // if yes give a alert and halt process
+            // if not continue
+
+            $time = $data['time'];
+            $durationMinutes = $data['duration'];
+
+            $newScheduleStartTime = Carbon::now()->setTimeFromTimeString($time);
+            $newScheduleEndTime = Carbon::now()->setTimeFromTimeString($time)->addMinutes($durationMinutes);
+
+            $existingScheduleStartTime = Carbon::now()->setTimeFromTimeString($schedule->time);
+            $existingScheduleEndTime = Carbon::now()->setTimeFromTimeString($schedule->time)->addMinutes($schedule->duration);
+
+            dd($existingScheduleEndTime, $existingScheduleStartTime);
+
+            $startTimeCollide = $newScheduleStartTime->between($existingScheduleStartTime, $existingScheduleEndTime);
+            $endTimeCollide = $newScheduleEndTime->between($existingScheduleStartTime, $existingScheduleEndTime);
+
+            if ($startTimeCollide || $endTimeCollide) {
+                Notification::make()
+                    ->title('Schedule collision')
+                    ->warning()
+                    ->send();
+                $this->halt();
+            }
+        });
+
+        return $data;
+    }
+}
